92d00c69aaa845dd6190fdd78e8615962ebbf38b
mihkhoi <minhkhoi532009@gmail.com>
2023-08-08 23:25:38 +0700

    bai tap

---
+++ Chuong 5 mang 1 chieu/Ky thuat dat linh canh cac bai tap co ban/140.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void nhap(double a[], int n)
+{
+    for(int i = 0;i < n;i++)
+    {
+        cin >> a[i];
+    }
+}
+
+double solve(double a[], int n)
+{
+    double min = 1000;
+    for(int i = 0;i < n;i++)
+    {
+        if(a[i] > 0)
+        {
+            if(a[i] < min)
+            {
+                min = a[i];
+            }
+        }
+        else
+        {
+            continue;
+        }
+    }
+    if(min == 1000)
+    {
+        return -1;
+    }
+    return min;
+}
+
+int main()
+{
+    double a[100];
+    int n;
+    cin >> n;
+    nhap(a, n);
+    cout << solve(a, n);
+    return 0;
+}
