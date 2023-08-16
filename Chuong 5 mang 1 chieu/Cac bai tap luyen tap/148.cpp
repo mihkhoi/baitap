c188587f12faa54ac3a15de41dfe584c05b1ccfb
mihkhoi <minhkhoi532009@gmail.com>
2023-08-16 16:31:20 +0700

    bai tap

---
+++ Chuong 5 mang 1 chieu/Cac bai tap luyen tap/148.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void nhap(int a[], int n)
+{
+    for(int i = 0;i < n;i++)
+    {
+        cin >> a[i];
+    }
+}
+
+bool check(int n)
+{
+    if(n < 2)
+    {
+        return false;
+    }
+    for(int i = 2;i <= sqrt(n);i++)
+    {
+        if(n%i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int solve(int a[], int n)
+{
+    int ntcc = -1;
+    for(int i = 0;i < n;i++)
+    {
+        if(check(a[i]))
+        {
+            ntcc = a[i];
+        }
+    }
+    return ntcc;
+}
+
+int main()
+{
+    int a[100], n;
+    cin >> n;
+    nhap(a, n);
+    cout << solve(a, n);
+    return 0;
+}
