f3e37c7e4f339c7c89e70c8e47f75c4f91b146ac
mihkhoi <minhkhoi532009@gmail.com>
2023-08-17 20:16:46 +0700

    bai tap

--- Chuong 5 mang 1 chieu/Cac bai tap luyen tap/153.cpp
+++ Chuong 5 mang 1 chieu/Cac bai tap luyen tap/153.cpp
@@ -11,16 +11,21 @@ void nhap(int a[], int n)
 
 int solve(int a[], int n)
 {
-    int min = a[n - 1];
+    int min = a[n - 1],  d = 0;
     for(int i = 0;i < n;i++)
     {
         if(a[i]%2 == 0)
         {
             if(a[i] < min)
             {
+                d++;
                 min = a[i];
             }
         }
+    }
+    if(d == 0)
+    {
+        return -1;
     }
     return min;
 }
@@ -29,5 +34,7 @@ int main()
 {
     int a[100], n;
     cin >> n;
+    nhap(a, n);
+    cout << solve(a, n);
     return 0;
 }
